da597e965cdc3dc6af7daf4f011fcc699f8ac2d7
gina7484 <gina7484@naver.com>
2020-10-10 05:17:59 +0900

    Organize pages into hamburger menu

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -91,7 +91,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Montserrat',
           primarySwatch: Colors.blue,
           primaryColor: persian_blue),
-      home: MyHomePage(title: 'Contra Flutter Kit Demo'),
+      home: MyHomePage(title: 'Mukgo Project'),
       routes: {
         '/project_all': (context) => ProjectPageMain(),
         '/project_map': (context) => MapDetailPage(),
@@ -101,6 +101,8 @@ class MyApp extends StatelessWidget {
               isBarChart: false,
             ),
         '/project_login': (context) => LoginForm(),
+
+        '/sample_page': (context) => SamplePage(title:'Sample pages'),
         '/onboard_all': (context) => OnboardPageMain(),
         '/onboard_type_one': (context) => OnboardingPagerTypeOne(),
         '/onboard_type_two': (context) => OnboardingPagerTypeTwo(),
@@ -199,19 +201,115 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      body: Center(child: Text('My Page!')),
+      floatingActionButton: Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: ButtonRoundWithShadow(
+              size: 60,
+              borderColor: wood_smoke,
+              color: white,
+              callback: () {
+                googleSignOut(context);
+              },
+              shadowColor: wood_smoke,
+              iconPath: "assets/icons/ic_add.svg"),
+        ),
+      ),
+      drawer:Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.all(20),
+              trailing: Icon(Icons.navigate_next),
+              title: Text("Map"),
+              onTap: () {
+                Navigator.pushNamed(context, "/project_map");
+              },
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.all(20),
+              trailing: Icon(Icons.navigate_next),
+              title: Text("Restaurant"),
+              onTap: () {
+                Navigator.pushNamed(context, "/project_restaurant");
+              },
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.all(20),
+              trailing: Icon(Icons.navigate_next),
+              title: Text("Review"),
+              onTap: () {
+                Navigator.pushNamed(context, "/project_review");
+              },
+            ),
+            Container(
+              child: ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("User"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/project_user");
+                },
+              ),
+            ),
+            Container(
+              child: ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("Login"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/project_login");
+                },
+              ),
+            ),
+            Container(
+              child: ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("Sample Pages"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/sample_page");
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SamplePage extends StatefulWidget {
+  SamplePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SamplePageState createState() => _SamplePageState();
+}
+class _SamplePageState extends State<SamplePage> {
+  @override
+  Widget build(BuildContext context) {
+    //checkAuth(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.all(10),
           child: Column(
             children: <Widget>[
-              ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Mukgo Project Pages"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/project_all");
-                },
-              ),
               ListTile(
                 contentPadding: EdgeInsets.all(20),
                 trailing: Icon(Icons.navigate_next),
@@ -369,6 +467,7 @@ class _MyHomePageState extends State<MyHomePage> {
               iconPath: "assets/icons/ic_add.svg"),
         ),
       ),
+ 
     );
   }
 }
