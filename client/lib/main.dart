10c4f990510a3d376988951089c6f204056a6d5c
gina7484 <gina7484@naver.com>
2020-10-15 06:30:54 +0900

    Create tab for map, my page & button for contra

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+
+//-------For Contra Sample Pages (START) ------//
 import 'package:contra/blog/blog_home_page.dart';
 import 'package:contra/blog/blog_list_page_four.dart';
 import 'package:contra/blog/blog_list_page_one.dart';
@@ -67,14 +69,17 @@ import 'package:contra/menu/settings_page_two.dart';
 import 'package:contra/onboarding/type1/pager.dart';
 import 'package:contra/onboarding/type2/pager.dart';
 import 'package:contra/onboarding/type4/onboard_page_four.dart';
-import 'package:contra/custom_widgets/button_round_with_shadow.dart';
+//-------For Contra Sample Pages (END) ------//
 
 import 'package:provider/provider.dart';
+import 'package:mukgo/project/sample_page.dart';
 import 'package:mukgo/auth/auth.dart';
 import 'package:mukgo/auth/auth_api.dart';
 import 'package:mukgo/auth/login_page.dart';
-import 'package:mukgo/project/project_main.dart';
+//import 'package:mukgo/project/project_main.dart';
+import 'package:mukgo/project/my_home_page.dart';
 import 'package:mukgo/map/map_detail.dart';
+import 'package:mukgo/user/user_detail.dart';
 import 'package:mukgo/review/review_form.dart';
 import 'package:mukgo/restaurant/restaurant_detail.dart';
 
@@ -93,7 +98,6 @@ class MyApp extends StatelessWidget {
           primaryColor: persian_blue),
       home: MyHomePage(title: 'Mukgo Project'),
       routes: {
-        '/project_all': (context) => ProjectPageMain(),
         '/project_map': (context) => MapDetailPage(),
         '/project_restaurant': (context) => RestaurantDetailPage(),
         '/project_review': (context) => ReviewForm(),
@@ -101,7 +105,9 @@ class MyApp extends StatelessWidget {
               isBarChart: false,
             ),
         '/project_login': (context) => LoginForm(),
-        '/sample_page': (context) => SamplePage(title: 'Sample pages'),
+        '/sample_page': (context)=> SamplePage(),
+
+        //-------For Contra Sample Pages (START) ------//
         '/onboard_all': (context) => OnboardPageMain(),
         '/onboard_type_one': (context) => OnboardingPagerTypeOne(),
         '/onboard_type_two': (context) => OnboardingPagerTypeTwo(),
@@ -174,299 +180,11 @@ class MyApp extends StatelessWidget {
         '/settings_type_one': (context) => SettingsPageOne(),
         '/settings_type_two': (context) => SettingsPageTwo(),
         '/settings_type_three': (context) => SettingsPageThree(),
+        //-------For Contra Sample Pages (END) ------//
       },
     );
 
     return ChangeNotifierProvider(
         create: (context) => AuthModel(), child: child);
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    checkAuth(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(child: Text('My Page!')),
-      floatingActionButton: Align(
-        alignment: Alignment.bottomRight,
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: ButtonRoundWithShadow(
-              size: 60,
-              borderColor: wood_smoke,
-              color: white,
-              callback: () {
-                googleSignOut(context);
-              },
-              shadowColor: wood_smoke,
-              iconPath: "assets/icons/ic_add.svg"),
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.all(20),
-              trailing: Icon(Icons.navigate_next),
-              title: Text("Map"),
-              onTap: () {
-                Navigator.pushNamed(context, "/project_map");
-              },
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.all(20),
-              trailing: Icon(Icons.navigate_next),
-              title: Text("Restaurant"),
-              onTap: () {
-                Navigator.pushNamed(context, "/project_restaurant");
-              },
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.all(20),
-              trailing: Icon(Icons.navigate_next),
-              title: Text("Review"),
-              onTap: () {
-                Navigator.pushNamed(context, "/project_review");
-              },
-            ),
-            Container(
-              child: ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("User"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/project_user");
-                },
-              ),
-            ),
-            Container(
-              child: ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Login"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/project_login");
-                },
-              ),
-            ),
-            Container(
-              child: ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Sample Pages"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/sample_page");
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class SamplePage extends StatefulWidget {
-  SamplePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _SamplePageState createState() => _SamplePageState();
-}
-
-class _SamplePageState extends State<SamplePage> {
-  @override
-  Widget build(BuildContext context) {
-    //checkAuth(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Onboarding and Splash"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/onboard_all");
-                },
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Forms, Login, Signup"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/login_all");
-                },
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Chatting Screens"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/chat_home");
-                },
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Shopping Screens"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/shopping_main_page");
-                },
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Blog Screens"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/blog_main_page");
-                },
-              ),
-              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Payment"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/payment_main_page");
-                  },
-                ),
-              ),
-              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Alarm, Clock, Weather"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/alarm_main_page");
-                  },
-                ),
-              ),
-              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Data and Statistics"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/chart_main_page");
-                  },
-                ),
-              ),
-              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Location And Map"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/map_main_page");
-                  },
-                ),
-              ),
-              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Content, Text Details"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/content_text_main_page");
-                  },
-                ),
-              ),
-              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Menu and Settings"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/menu_settings_main_page");
-                  },
-                ),
-              ),
-/*              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Dialogs, Filters, Toasts"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/empty_state");
-                  },
-                ),
-              ),
-              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Profile"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/empty_state");
-                  },
-                ),
-              ),
-              Container(
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(20),
-                  trailing: Icon(Icons.navigate_next),
-                  title: Text("Menus"),
-                  onTap: () {
-                    Navigator.pushNamed(context, "/empty_state");
-                  },
-                ),
-              ),
-              ListTile(
-                contentPadding: EdgeInsets.all(20),
-                trailing: Icon(Icons.navigate_next),
-                title: Text("Profile"),
-                onTap: () {
-                  Navigator.pushNamed(context, "/empty_state");
-                },
-              )*/
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: Align(
-        alignment: Alignment.bottomRight,
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: ButtonRoundWithShadow(
-              size: 60,
-              borderColor: wood_smoke,
-              color: white,
-              callback: () {
-                googleSignOut(context);
-              },
-              shadowColor: wood_smoke,
-              iconPath: "assets/icons/ic_add.svg"),
-        ),
-      ),
-    );
   }
 }
---
+++ client/lib/project/sample_page.dart
@@ -0,0 +1,190 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:contra/onboarding/onboard_main.dart';
+
+class SamplePage extends StatefulWidget {@override
+  _SamplePageState createState() => _SamplePageState();
+}
+
+class _SamplePageState extends State<SamplePage> {
+  @override
+  Widget build(BuildContext context) {
+    //checkAuth(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('contra sample page'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("Onboarding and Splash"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/onboard_all");
+                  /*
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => OnboardPageMain()),
+                  );
+                  */
+                },
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("Forms, Login, Signup"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/login_all");
+                },
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("Chatting Screens"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/chat_home");
+                },
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("Shopping Screens"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/shopping_main_page");
+                },
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("Blog Screens"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/blog_main_page");
+                },
+              ),
+              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Payment"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/payment_main_page");
+                  },
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Alarm, Clock, Weather"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/alarm_main_page");
+                  },
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Data and Statistics"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/chart_main_page");
+                  },
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Location And Map"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/map_main_page");
+                  },
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Content, Text Details"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/content_text_main_page");
+                  },
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Menu and Settings"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/menu_settings_main_page");
+                  },
+                ),
+              ),
+/*              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Dialogs, Filters, Toasts"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/empty_state");
+                  },
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Profile"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/empty_state");
+                  },
+                ),
+              ),
+              Container(
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(20),
+                  trailing: Icon(Icons.navigate_next),
+                  title: Text("Menus"),
+                  onTap: () {
+                    Navigator.pushNamed(context, "/empty_state");
+                  },
+                ),
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.all(20),
+                trailing: Icon(Icons.navigate_next),
+                title: Text("Profile"),
+                onTap: () {
+                  Navigator.pushNamed(context, "/empty_state");
+                },
+              )*/
+            ],
+          ),
+        ),
+      ),
+      /*
+      floatingActionButton: Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: ButtonRoundWithShadow(
+              size: 60,
+              borderColor: wood_smoke,
+              color: white,
+              callback: () {
+                googleSignOut(context);
+              },
+              shadowColor: wood_smoke,
+              iconPath: "assets/icons/ic_add.svg"),
+        ),
+      ),
+      */
+    );
+  }
+}
