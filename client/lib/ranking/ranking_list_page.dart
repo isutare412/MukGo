1827c7d8935e13e841561ee700482cbb11d3c4f4
niceandneat <ddackmin@gmail.com>
2020-11-21 14:13:29 +0900

    Add ranking refresh

--- client/lib/ranking/ranking_list_page.dart
+++ client/lib/ranking/ranking_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:contra/login/contra_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,54 +15,72 @@ class RankingListPage extends StatefulWidget {
 }
 
 class _RankingListPageState extends State<RankingListPage> {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
   List<RankingUser> users = [];
+
+  void _update() async {
+    var rawUsers = await fetchRankingData(readAuth(context).token);
+    setState(() {
+      users = rawUsers.users.asMap().entries.map((entry) {
+        var idx = entry.key;
+        var user = entry.value;
+        return RankingUser(
+            name: user.name,
+            level: user.level,
+            reviewCount: user.reviewCount,
+            asset: levelToProfileAsset(user.level),
+            rank: idx + 1);
+      }).toList();
+    });
+  }
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await _update();
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
 
   @override
   void initState() {
     super.initState();
-    Future.microtask(() async {
-      var rawUsers = await fetchRankingData(readAuth(context).token);
-      setState(() {
-        users = rawUsers.users.asMap().entries.map((entry) {
-          var idx = entry.key;
-          var user = entry.value;
-          return RankingUser(
-              name: user.name,
-              level: user.level,
-              reviewCount: user.reviewCount,
-              asset: levelToProfileAsset(user.level),
-              rank: idx + 1);
-        }).toList();
-      });
+    Future.microtask(() {
+      _update();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Padding(
-              padding: const EdgeInsets.all(
-                24,
-              ),
-              child: ContraText(
-                size: 30.0,
-                text: 'Ranking',
-                alignment: Alignment.centerLeft,
-              )),
-          ListView.builder(
-              shrinkWrap: true,
-              padding: EdgeInsets.all(24),
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: users.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RankingListItem(
-                  user: users[index],
-                );
-              })
-        ],
-      ),
-    );
+    return SmartRefresher(
+        enablePullDown: true,
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.all(
+                    24,
+                  ),
+                  child: ContraText(
+                    size: 30.0,
+                    text: 'Ranking',
+                    alignment: Alignment.centerLeft,
+                  )),
+              ListView.builder(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.all(24),
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: users.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return RankingListItem(
+                      user: users[index],
+                    );
+                  })
+            ],
+          ),
+        ));
   }
 }
